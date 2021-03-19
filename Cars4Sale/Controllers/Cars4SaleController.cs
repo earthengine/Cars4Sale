5831bd085f130c02b3c5bed5950b87c12e9dea32
Joe Ren <earthengine@hotmail.com>
2021-03-19 17:27:38 +1100

    Add Controller

---
+++ Cars4Sale/Controllers/Cars4SaleController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Cars4Sale.Models;
+
+namespace Cars4Sale.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Cars4SaleController : ControllerBase
+    {
+        private readonly ILogger<Cars4SaleController> _logger;
+
+        public Cars4SaleController(ILogger<Cars4SaleController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Get all cars in a list
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<Car> Get()
+        {
+            return Cars.Get().ToArray();
+        }
+    }
+}
