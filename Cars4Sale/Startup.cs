3cb4363fac6d7200abc6bff89211caa508cae7e9
Joe Ren <earthengine@hotmail.com>
2021-03-19 17:31:46 +1100

    Allow XML comments in Swagger and change endpoint

--- Cars4Sale/Startup.cs
+++ Cars4Sale/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace Cars4Sale
 {
@@ -26,25 +21,37 @@ namespace Cars4Sale
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cars4Sale", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                    Title = "Cars4Sale",
+                    Version = "v1",
+                    Description = "API for accessing Cars4Sale",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Joe Ren",
+                        Email = "joe.ren@gmail.com",
+                    }
+                });
+                var name = Assembly.GetExecutingAssembly().GetName().Name;
+                var xmlFile = $"{name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cars4Sale v1"));
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger(c => c.RouteTemplate = "api/Cars4Sale/{documentname}/swagger.json");
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/api/Cars4Sale/v1/swagger.json", "Cars4Sale v1");
+                c.RoutePrefix = "api/Cars4Sale";
+            });
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
