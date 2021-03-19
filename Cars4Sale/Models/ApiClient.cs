b3badcf73667a58da43a30f785bb72c02bae839d
Joe Ren <earthengine@hotmail.com>
2021-03-19 17:26:30 +1100

    Add models

---
+++ Cars4Sale/Models/ApiClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Cars4Sale.Models
+{
+    public class ApiClient
+    {
+        public string ApiKey { get; set; }
+        public static IList<ApiClient> Clients = new List<ApiClient>
+        {
+            new ApiClient { ApiKey = "DDF53D65-E63B-4677-A0F5-DE3D8AE576AE" },
+            new ApiClient { ApiKey = "03C80182-D7D7-4732-92EC-3FCC898EB706" }
+        }.AsReadOnly();
+    }
+}
---
+++ Cars4Sale/Models/Car.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+
+namespace Cars4Sale.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Stock { get; set; }
+        [JsonIgnore]
+        public ApiClient Client { get; set; }
+        [JsonIgnore]
+        public bool Deleted { get; set; } = false;
+    }
+
+    public class Cars { 
+
+        private static ConcurrentDictionary<int, Car> _Cars = new ConcurrentDictionary<int, Car>(new Dictionary<int, Car> {
+            { 1, new Car { Id = 1, Make = "Toyota", Model = "Corola", Year = 2015, Stock = 20, Client = ApiClient.Clients[0] } }
+        });
+
+        public static ICollection<Car> Get()
+        {
+            return _Cars.Values;
+        }
+
+        public static void AddCar(Car car)
+        {
+            var cnt = _Cars.Count;
+            car.Id = cnt;
+            while(_Cars.GetOrAdd(cnt, car) != car) {
+                cnt++;
+                car.Id = cnt;
+            }
+        }
+
+        public void RemoveCar(ApiClient current_client, int id)
+        {
+        }
+    }
+}
