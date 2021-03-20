02f2c679cfd1ac121891a134d302f8ced7b6cfd6
Joe Ren <earthengine@hotmail.com>
2021-03-20 14:25:04 +1100

    Add JWT ApiKey attribute

---
+++ Cars4Sale/Attributes/ApiKeyAttribute.cs
@@ -0,0 +1,47 @@
+﻿using Cars4Sale.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Threading.Tasks;
+
+namespace Cars4Sale.Attributes
+{
+    public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string APIKEYNAME = "ApiKey";
+
+        public bool Optional = false;
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var headers = context.HttpContext.Request.Headers;
+
+            if (await CheckCriteria(!headers.TryGetValue(APIKEYNAME, out var extractedApiKey),
+                    "Api Key was not provided", context, next)) { return; }
+
+            if (await CheckCriteria(!Guid.TryParse(extractedApiKey.ToString(), out var apiKey),
+                    "Api Key is not valid", context, next)) { return; }
+
+            if (await CheckCriteria(!ApiClient.Clients.ContainsKey(apiKey),
+                    "Api Key is not valid", context, next)) { return; }
+
+            context.HttpContext.Items.Add("current_client", ApiClient.Clients[apiKey]);
+            await next();
+        }
+
+        private async Task<bool> CheckCriteria(bool criteria, string msg, ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (criteria)
+            {
+                if (Optional)
+                {
+                    await next();                    
+                } else
+                {
+                    context.Result = ApiError.Unauthorized(msg).ToContentResult();
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
