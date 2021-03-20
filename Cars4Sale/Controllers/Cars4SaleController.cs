f1e98b22db252bf570e5f8f44c1d16446f084b59
Joe Ren <earthengine@hotmail.com>
2021-03-20 14:25:45 +1100

    Implement business logic

--- Cars4Sale/Controllers/Cars4SaleController.cs
+++ Cars4Sale/Controllers/Cars4SaleController.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cars4Sale.Attributes;
+using Cars4Sale.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-
-using Cars4Sale.Models;
 
 namespace Cars4Sale.Controllers
 {
@@ -20,13 +20,112 @@ namespace Cars4Sale.Controllers
         }
 
         /// <summary>
-        /// Get all cars in a list
+        /// Get all cars in a list.
         /// </summary>
-        /// <returns></returns>
+        /// <param name="model">The car model to search, optional.</param>
+        /// <param name="make">The car make to search, optional.</param>
+        /// <param name="all">Set to true to get all vehicle details including from other dealers; false to only return cars for the current dealer.</param>
+        /// <returns>The list of cars</returns>
         [HttpGet]
-        public IEnumerable<Car> Get()
+        [ApiKey(Optional = true)]
+        [ProducesResponseType(typeof(Car[]), StatusCodes.Status200OK)]
+        public IActionResult GetList(string model = null, string make = null, bool all = false)
         {
-            return Cars.Get().ToArray();
+            var current_client = HttpContext.Items["current_client"] as ApiClient;
+            if (current_client == null) all = true;
+            return Ok(Cars.Get()
+                          .Where(x => model!=null ? x.Model.Contains(model) : true)
+                          .Where(x => make != null ? x.Model.Contains(make) : true)
+                          .Where(x => all ? true : x.Client == current_client));
+        }
+
+        /// <summary>
+        /// Get information of a single car.
+        /// </summary>
+        /// <param name="car_id">The car's ID to get information from.</param>
+        /// <returns>The list of cars</returns>
+        /// <response code="404">There is no car with the specified ID exists in the system.</response>
+        [HttpGet]
+        [Route("{car_id}")]
+        [ProducesResponseType(typeof(Car), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+        public IActionResult Get(Guid car_id)
+        {
+            return Ok(Cars.Get(car_id));
+        }
+
+        /// <summary>
+        /// Add a car to the current API client.
+        /// </summary>
+        /// <param name="new_car">The new car data</param>
+        /// <returns>The new car with the ID allocated.</returns>
+        /// <response code="400">The car data is no valid. Check the resulting string for details.</response>
+        /// <response code="500">Something weird happened that stops the system accepting the car data. Contact Cars4Sale for support.</response>
+        [HttpPost]
+        [ApiKey]
+        [ProducesResponseType(typeof(Car), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+        public IActionResult Add(NewCar new_car)
+        {
+            var current_client = HttpContext.Items["current_client"] as ApiClient;
+
+            (var car, var maybe_error) = Cars.AddCar(current_client, new_car);
+            if (maybe_error is ApiError error)
+            {
+                return error.ToContentResult();
+            }
+            return Ok(car);
+        }
+
+        /// <summary>
+        /// Add a car to the current API client.
+        /// </summary>
+        /// <param name="car_id">The ID for the car to be removed</param>
+        /// <returns>A JSON object includes the car that was removed</returns>
+        /// <response code="403">Either the car you are going to remove is not belong to you the dealer, or the car does not exist.</response>
+        /// <response code="410">The car you are trying to remove was removed already by another similar request.</response>
+        [HttpDelete]
+        [Route("{car_id}")]
+        [ProducesResponseType(typeof(Guid), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status410Gone)]
+        public IActionResult Remove(Guid car_id)
+        {
+            var current_client = HttpContext.Items["current_client"] as ApiClient;
+
+            var (car, maybe_error) = Cars.RemoveCar(current_client, car_id);
+            if (maybe_error is ApiError error)
+            {
+                return error.ToContentResult();
+            }
+            return Ok(new { Removed = car });
+        }
+
+        /// <summary>
+        /// Update the car stock value
+        /// </summary>
+        /// <param name="car_id">The ID for the car to be updated</param>
+        /// <param name="new_stock">The new stock value to set</param>
+        /// <returns>A JSON object includes the car ID, the old stock value and the new</returns>
+        /// <response code="403">Either the car you are going to remove is not belong to you the dealer, or the car does not exist.</response>
+        /// <response code="410">A similar request attempted to update the stock. Please try again.</response>
+        [HttpPatch]
+        [Route("{car_id}")]
+        [ApiKey]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public IActionResult UpdateStock(Guid car_id, [FromBody] int new_stock)
+        {
+            var current_client = HttpContext.Items["current_client"] as ApiClient;
+
+            var (old_stock, maybe_error) = Cars.UpdateStock(current_client, car_id, new_stock);
+            if (maybe_error is ApiError error)
+            {
+                return error.ToContentResult();
+            }
+            return Ok(new { Id = car_id, From = old_stock, To = new_stock });
         }
     }
 }
--- Cars4Sale/Models/ApiClient.cs
+++ Cars4Sale/Models/ApiClient.cs
@@ -1,14 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Cars4Sale.Models
 {
     public class ApiClient
     {
-        public string ApiKey { get; set; }
-        public static IList<ApiClient> Clients = new List<ApiClient>
+        public Guid ApiKey { get; set; }
+        public string DealerName { get; set; }
+        public static IReadOnlyDictionary<Guid, ApiClient> Clients = new Dictionary<Guid, ApiClient>
         {
-            new ApiClient { ApiKey = "DDF53D65-E63B-4677-A0F5-DE3D8AE576AE" },
-            new ApiClient { ApiKey = "03C80182-D7D7-4732-92EC-3FCC898EB706" }
-        }.AsReadOnly();
+            {new Guid("DDF53D65-E63B-4677-A0F5-DE3D8AE576AE"), new ApiClient { ApiKey = new Guid("DDF53D65-E63B-4677-A0F5-DE3D8AE576AE"), DealerName = "CarsRus" } },
+            {new Guid("03C80182-D7D7-4732-92EC-3FCC898EB706"), new ApiClient { ApiKey = new Guid("03C80182-D7D7-4732-92EC-3FCC898EB706"), DealerName = "ECars" } }
+        };
     }
 }
